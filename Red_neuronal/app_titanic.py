a36369b6247c597a052ca57c877c063893b54fbf
JavierMunBall <javier.munbal@gmail.com>
2023-11-27 13:25:20 +0100

    Update app_titanic.py

--- Red_neuronal/app_titanic.py
+++ Red_neuronal/app_titanic.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from tensorflow import keras
 import tensorflow as tf
+from tensorflow import keras
 from sklearn.preprocessing import StandardScaler
 
 # Cargar el modelo guardado
